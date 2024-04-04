8a438ad3f01a448f5d045b7875c3fb7c1de2228c
VolkerFelix <volker.leukhardt@gmail.com>
2024-04-04 21:05:05 +0100

    DAX scraping implemented

--- valuation/app/main.py
+++ valuation/app/main.py
@@ -9,11 +9,6 @@ import argparse
 import pprint
 import warnings
 
-INDEX_URL_MAP = {
-    'SP500': 'https://stockanalysis.com/list/sp-500-stocks',
-    'DAX': 'https://en.wikipedia.org/wiki/DAX'
-}
-
 # Time span in years for valuation
 TIME_SPAN = 5
 
@@ -43,13 +38,8 @@ def run_based_on_index_name(
         f_verbose: bool,
         f_bearer_token: str = ''
     ):
-    try:
-        index_url = INDEX_URL_MAP[f_index_name]
-    except KeyError:
-        print("Index name not found.")
-
     stock_counter = 0
-    for stock_name, symbol in CompaniesList(index_url).m_companies.items():
+    for stock_name, symbol in CompaniesList(f_index_name).m_companies.items():
         stock = run_analysis(symbol, stock_name)
         stock_counter += 1
         if f_verbose:
--- valuation/app/web_scrape/companiesList.py
+++ valuation/app/web_scrape/companiesList.py
@@ -1,36 +1,62 @@
 from bs4 import ResultSet
 from web_scrape.scrape import scrape_table
 
-# Simulate browser
-HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'
+INDEX_URL_MAP = {
+    'SP500': 'https://stockanalysis.com/list/sp-500-stocks',
+    'DAX': 'https://en.wikipedia.org/wiki/DAX'
 }
 
-def get_all_companies(f_index_url: str) -> ResultSet:
-    if 'DAX' in f_index_url:
-         return scrape_table(f_index_url, "constituents")
+def __dax_get_names_and_symbols(f_scrapped_table: ResultSet) -> dict:
+    result = {}
+    for table in f_scrapped_table:
+        # Scrape all rows
+        trs = table.find_all('tr')
+        for tr in trs:
+            # Scrape all table data tags
+            tds = tr.find_all('td')
+            # Index 1 of tds contains the company's name
+            # Index 3 of tds contains the company's symbol
+            if not tds:
+                continue
+            name = tds[1].get_text()
+            symbol = tds[3].get_text()
+            result[name] = symbol
+
+    return result
+
+def __sp500_get_names_and_symbols(f_scrapped_table: ResultSet) -> dict:
+    result = {}
+    for table in f_scrapped_table:
+        trs = table.find_all('tr')
+        for tr in trs:
+            tds = tr.find_all('td')
+            if not tds:
+                continue
+            name = tds[2].get_text().strip()
+            symbol = tds[1].get_text()
+            symbol = symbol.replace('.','-')
+            result[name] = symbol
+    
+    return result
+
+def get_all_companies(f_index_name: str, f_index_url: str) -> dict:
+    result = {}
+    if f_index_name == 'DAX':
+        result = __dax_get_names_and_symbols(scrape_table(f_index_url, "constituents"))
+    elif f_index_name == 'SP500':
+        result =  __sp500_get_names_and_symbols(scrape_table(f_index_url))
     else:
-        return scrape_table(f_index_url)    
+        assert False, f"Index {f_index_name} not implemented."
+    
+    return result
 
 class CompaniesList:
-    def __init__(self, f_list_name: str):
+    def __init__(self, f_index_name: str):
         self.m_companies = {}
-        self._load_names_and_symbols(f_list_name)
-
-    def _load_names_and_symbols(self, f_list_name: str) -> None:
-        scraped_data = get_all_companies(f_list_name)
-        for table in scraped_data:
-            # Scrape all table rows into variable trs
-            trs = table.find_all('tr')
-            for tr in trs:
-                # Scrape all table data tags into variable tds
-                tds = tr.find_all('td')
-                # Index 0 of tds will contain the measurement
-                # Index 1 of tds will contain the value
-                if not tds:
-                    continue
-                name = tds[2].get_text().strip()
-                symbol = tds[1].get_text()
-                symbol = symbol.replace('.','-')
-                self.m_companies[name] = symbol
+        index_url = ''
+        try:
+            index_url = INDEX_URL_MAP[f_index_name]
+        except KeyError:
+            print("Index name not found.")
+        self.m_companies = get_all_companies(f_index_name, index_url)
 
