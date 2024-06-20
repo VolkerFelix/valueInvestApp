4f51d500ed4ed9b6b7a11c3d507001bcc467b969
VolkerFelix <volker.leukhardt@gmail.com>
2024-06-20 21:55:09 +0100

    Added cherry picker to compose

--- cherry_picker/app/main.py
+++ cherry_picker/app/main.py
@@ -1,7 +1,9 @@
+import os
 from db_interface.stock_db_interface import request_access_token, get_stocks
 from find_cherries.findCherries import findCherries
 
-WEBHOOK_URL_PROD = "https://discord.com/api/webhooks/1224779729136255128/ACRhjlv6NED9_H-g6_0oTgWH7EMwt_PObry403HCSjWb5u79PGOcnjfyTli_7GztV4tT"
+SP500_WEBHOOK = os.getenv('SP500_WEBHOOK')
+DAX_WEBHOOK = os.getenv('DAX_WEBHOOK')
 
 if __name__ == '__main__':
     bearer = ''
@@ -10,5 +12,5 @@ if __name__ == '__main__':
         respone = request_access_token()
         bearer = respone.json()['access_token']
     stocks = get_stocks(bearer)
-    cherry_finder = findCherries(stocks, WEBHOOK_URL_PROD)
+    cherry_finder = findCherries(stocks, SP500_WEBHOOK)
     cherry_finder.find_undervalued_stocks()
